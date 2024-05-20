b9db088e538c19eb7ec8583f27c3f7e74e58d00d
Andres Viana Gonzalez <andresvgonzalez@Andress-MacBook-Pro-2.local>
2024-05-20 14:44:55 -0500

    remove cache response from database

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -6,36 +6,13 @@ if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
 }
 
-// Define the global cache if it doesn't exist
-declare global {
-  var mongoose: {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
-  };
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 async function connectToDatabase(): Promise<Mongoose> {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  const opts = {
+    bufferCommands: false,
+  };
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
+  const connection = await mongoose.connect(MONGODB_URI, opts);
+  return connection;
 }
 
 export default connectToDatabase;
